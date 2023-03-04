dcad569248df3914b978ea5986f49a1374715b93
greymesa <58270183+greymesa@users.noreply.github.com>
2023-03-04 15:21:35 -0500

    praying that this works

---
+++ js/trial.js
@@ -0,0 +1,21 @@
+fetch("https://mindmedic.hexagon.monster/api/trialconvo", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({id:"random",message:"hi"})
+})
+
+// Get the input field
+var input = document.getElementById("ai-trial");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+});
